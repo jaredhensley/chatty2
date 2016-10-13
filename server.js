02585ae2a62abb5a3d1337d133fc1da47d403194
Jared Hensley <jahensle85@me.com>
2016-10-12 23:05:06 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
+var app = express();
+var port = 8000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+var messages = [];
+
+app.get('/', function(req, res, next) {
+  res.send(JSON.stringify(messages));
+});
+
+app.post('/', function(req, res, next) {
+
+  var message = {
+    message: req.body.message,
+    date: new Date()
+  }
+  messages.push(message);
+  res.send(JSON.stringify(messages));
+});
+
+app.listen(port, function() {
+  console.log('listening on port', port);
+});
